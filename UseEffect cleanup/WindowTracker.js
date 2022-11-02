e98b79b10ed41c8a56b46c58f087077262bfca15
sirgama <sirengogamaliel@gmail.com>
2022-11-02 11:00:14 +0300

    add react useeffect project

---
+++ UseEffect cleanup/WindowTracker.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+export default function WindowTracker() {
+    
+    const [windowWidth, setWindowWidth] = React.useState(window.innerWidth)
+    
+    React.useEffect(() => {
+        function watchWidth() {
+            console.log("Setting up...")
+            setWindowWidth(window.innerWidth)
+        }
+        
+        window.addEventListener("resize", watchWidth)
+        
+        return function() {
+            console.log("Cleaning up...")
+            window.removeEventListener("resize", watchWidth)
+        }
+    }, [])
+    
+    return (
+        <h1>Window width: {windowWidth}</h1>
+    )
+}
